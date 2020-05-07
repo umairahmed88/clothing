39c0e00ceec73b98aa3bd085dfdc7ddfee4f4cd3
umairahmed88 <umairahmed_88@ymail.com>
2020-05-07 20:06:21 +0500

    added custom button

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -9,7 +9,7 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg.sv
 import './cart-icon.styles.scss'
 
 const CartIcon = ({ toggleCartHidden }) => (
-    <div className='cart-icon' onClick={toggleCartHidden()}>
+    <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
         <span className='item-count'>0</span>
     </div>
